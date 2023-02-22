c411f87943d5cc4c311173d1984a829e1ea07baf
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-02-22 18:14:56 +0300

    22 February

---
+++ react-lesson06/rc06_forms-otherEvents/src/App.js
@@ -0,0 +1,24 @@
+// import Form from "./components/1-forms/Form"
+// import FormWithObject from "./components/1-forms/FormWithObject"
+// import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
+import Form2 from "./components/1-forms/Form2"
+import MouseEvent from "./components/3-mouseEvents/MouseEvent"
+
+
+
+function App() {
+  return (
+    <div className="container mt-4 text-center">
+
+      <Form2/>
+
+
+      {/* <Form /> */}
+      {/* <FormWithObject /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
+    </div>
+  )
+}
+
+export default App
---
+++ react-lesson06/rc06_forms-otherEvents/src/components/1-forms/Form2.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+
+const Form2 = () => {
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleUsername = (e) => {
+        // console.log(e.target.value);
+        setUsername(e.target.value)
+    }
+
+
+
+  return (
+    <form>
+        <h2 className="display-6 text-danger">Froms</h2>
+
+    <div className="mb-3">
+      <label htmlFor="username" className="form-label">Username: <span>{username}</span></label>
+      <input type="text" className="form-control" id="username" aria-describedby="emailHelp" onChange={handleUsername} required/>
+    </div>
+
+    <div className="mb-3">
+      <label htmlFor="eemail" className="form-label">Email address: <span>{email}</span></label>
+      <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} required/>
+    </div>
+    
+    <div className="mb-3">
+      <label htmlFor="password" className="form-label">Password: {password}</label>
+      <input type="password" className="form-control" id="password" onChange={(e) => setPassword(e.target.value)} required/>
+    </div>
+
+    <button type="submit" className="btn btn-danger">Submit</button>
+  </form>
+  )
+}
+
+export default Form2
