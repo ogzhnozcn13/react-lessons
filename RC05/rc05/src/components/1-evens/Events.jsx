5dca7eb1add9a3001e107c75e02297745d5fa1fc
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-04-13 14:13:36 +0300

    13 April

--- RC05/rc05/src/components/1-evens/Events.jsx
+++ RC05/rc05/src/components/1-evens/Events.jsx
@@ -1,9 +1,26 @@
 
 
 const Events = () => {
+
+    const handleClick = () => {
+        alert("clicked")
+    }
+
+    const handleClear = (msg) => {
+        window.confirm(msg)
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target);
+    }
+
   return (
-    <div>
-        <button>Click</button>
+    <div className="container text-center mt-4">
+        <button onClick={() => handleClick()} className="btn btn-success">Click</button>
+
+        <button  onClick={() => handleClear("you want this?")} className="btn btn-danger">Clear</button>
+
+        <button onClick={(e) => handleChange(e)} className="btn btn-warning" >Change</button>
     </div>
   )
 }
