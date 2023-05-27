24eedf9253778024ffe98267f334ab007afe1425
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-05-27 20:28:12 +0300

    27 May

--- RC06/src/components/3-mauseEvents/MauseEvents.jsx
+++ RC06/src/components/3-mauseEvents/MauseEvents.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 
 const MauseEvents = () => {
+    const [visible, setVisible] = useState(false)
+    // let visible = false
+    const [toggle, setToggle] = useState(false)
+
+    const handleDoubleClick = (e) => {
+        setToggle(!toggle)
+        toggle
+        ? (e.target.className = "bg-success text-light w-50 p-4 mt-4")
+        : (e.target.className = "bg-danger text-light w-50 p-4 mt-4")
+    }
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
     <h2 className="text-danger">MOUSE EVENTS</h2>
@@ -8,13 +19,17 @@ const MauseEvents = () => {
     <div
       id="todo-1"
       className="bg-success text-light w-50 p-4"
-      onMouseEnter={} onMouseOut={}>
+      onMouseEnter={() => {setVisible(true)}}
+      onMouseOut={() => {setVisible(false)}}>
       todo item 1
     </div>
 
+     {visible && <div>I SEE YOU</div>}
+
     <div
       id="todo-2"
-      className="bg-success text-light w-50 p-4 mt-4">
+      className="bg-success text-light w-50 p-4 mt-4"
+      onDoubleClick = {handleDoubleClick}>
       todo item 2
     </div>
 
